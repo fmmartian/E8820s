90c757cd6af61b4632c0c151da0c4f2fb9dff8bd
fmmartian <62557937+fmmartian@users.noreply.github.com>
2022-07-07 14:19:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-adguardhome
 #git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-aliddns
-git clone https://github.com/kenzok8/openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages
 git clone https://github.com/ntlf9t/luci-app-easymesh
+git clone https://github.com/liuran001/openwrt-packages.git
